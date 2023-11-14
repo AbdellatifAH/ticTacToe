6901ad9079f7a384ff6b2324a1997f8f8de081fa
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-11-14 16:35:11 +0200

    Added ScreenController Constructor to display the Game.

--- jvs.js
+++ jvs.js
@@ -7,7 +7,7 @@ function Gameboard() {
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push(Cell(i, j));
+            board[i].push(Cell());
         }
     }
 
@@ -18,7 +18,7 @@ function Gameboard() {
     const getAvailableCells = () => availableCells;
 
     const placeMarker = (i, j, player) => {
-        if (board[i][j].getValue() === 0) {
+        if (board[i][j].getValue() === "") {
             board[i][j].playerMove(player);
             decreaseAvailableCells();
         }
@@ -37,14 +37,10 @@ function Gameboard() {
     };
 }
 
-function Cell(indexC, indexR) {
-    let value = 0;
+function Cell() {
+    let value = "";
     const playerMove = (player) => {
         value = player;
-    }
-
-    const indexOfCell = () => {
-        indexC, indexR;
     }
 
     const getValue = () => value;
@@ -52,7 +48,6 @@ function Cell(indexC, indexR) {
     return {
         playerMove,
         getValue,
-        indexOfCell,
     };
 }
 
@@ -87,28 +82,28 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
 
     const checkWinner = () => {
         const value = board.getBoardValues();
-        if (value[0][0] != 0 && value[0][0] == value[0][1] && value[0][0] == value[0][2])
+        if (value[0][0] != "" && value[0][0] == value[0][1] && value[0][0] == value[0][2])
             return true;
         else
-            if (value[1][0] != 0 && value[1][0] == value[1][1] && value[0][0] == value[1][2])
+            if (value[1][0] != "" && value[1][0] == value[1][1] && value[1][0] == value[1][2])
                 return true;
             else
-                if (value[2][0] != 0 && value[2][0] == value[2][1] && value[0][0] == value[2][2])
+                if (value[2][0] != "" && value[2][0] == value[2][1] && value[2][0] == value[2][2])
                     return true;
                 else
-                    if (value[0][0] != 0 && value[0][0] == value[1][0] && value[0][0] == value[2][0])
+                    if (value[0][0] != "" && value[0][0] == value[1][0] && value[0][0] == value[2][0])
                         return true;
                     else
-                        if (value[0][1] != 0 && value[0][1] == value[1][1] && value[0][1] == value[2][1])
+                        if (value[0][1] != "" && value[0][1] == value[1][1] && value[0][1] == value[2][1])
                             return true;
                         else
-                            if (value[0][2] != 0 && value[0][2] == value[1][2] && value[0][2] == value[2][2])
+                            if (value[0][2] != "" && value[0][2] == value[1][2] && value[0][2] == value[2][2])
                                 return true;
                             else
-                                if (value[0][0] != 0 && value[0][0] == value[1][1] && value[0][0] == value[2][2])
+                                if (value[0][0] != "" && value[0][0] == value[1][1] && value[0][0] == value[2][2])
                                     return true;
                                 else
-                                    if (value[0][2] != 0 && value[0][2] == value[1][1] && value[0][2] == value[2][0])
+                                    if (value[0][2] != "" && value[0][2] == value[1][1] && value[0][2] == value[2][0])
                                         return true
                                     else
                                         return false;
@@ -116,16 +111,16 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
 
     const playRound = (i, j) => {
         const availableCells = board.getAvailableCells();
-        console.log(
-            `placing ${getActivePlayer().name}'s marker into the cell ${i} ${j}...`
-        );
         board.placeMarker(i, j, getActivePlayer().marker);
         if (availableCells > board.getAvailableCells()) {
-            if (board.getAvailableCells() == 0)
-                return console.log("No Winner, it's a DRAW!!!")
+            if (board.getAvailableCells() == 0) {
+                console.log("No Winner, it's a DRAW!!!")
+                return ("draw")
+            }
             if (checkWinner()) {
                 printPlayerTurn();
-                return console.log(`${getActivePlayer().name} is the WINNER!!`);
+                console.log(`${getActivePlayer().name} is the WINNER!!`);
+                return ("win");
             }
             else {
                 switchPlayerTurn();
@@ -134,15 +129,66 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
         }
         else {
             printPlayerTurn();
-            return console.log("illegal move, try again");
+            console.log("illegal move, try again");
+            return ("illegal move, try again");
         }
     };
 
     printPlayerTurn();
     return {
         playRound,
-        getActivePlayer
+        getActivePlayer,
+        getBoard: board.getBoard
     };
 }
 
-const game = GameController();
+function ScreenController() {
+    const game = GameController();
+    const playerTunDiv = document.querySelector(".turn");
+    const resultDiv = document.querySelector(".result")
+    const boardDiv = document.querySelector(".board");
+
+    const changeResultDiv = (result) => {
+        resultDiv.textContent = result;
+    }
+
+    const updateScreen = () => {
+        boardDiv.textContent = "";
+        const board = game.getBoard();
+        const activePlayer = game.getActivePlayer();
+
+        playerTunDiv.textContent = `${activePlayer.name}'s turn...`;
+
+        board.forEach((row, indexR) => {
+            row.forEach((cell, indexC) => {
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                cellButton.dataset.rowIndex = indexR
+                cellButton.dataset.columnIndex = indexC
+                cellButton.textContent = cell.getValue();
+                // if (cellButton.textContent == 0)
+                //     cellButton.addEventListener("click", clickHandlerBoard)
+                boardDiv.appendChild(cellButton);
+            })
+        })
+    }
+    function clickHandlerBoard(e) {
+        const indexR = e.target.dataset.rowIndex;
+        const indexC = e.target.dataset.columnIndex;
+        const gameResult = game.playRound(indexR, indexC);
+        if (!indexC || !indexR) return;
+        if (gameResult == "win") {
+            boardDiv.removeEventListener("click", clickHandlerBoard)
+            changeResultDiv(`${game.getActivePlayer().name} is the WINNER!!`)
+        }
+        else if (gameResult == "draw")
+            changeResultDiv("No Winner, it's a DRAW!!!")
+        updateScreen();
+    }
+    boardDiv.addEventListener("click", clickHandlerBoard);
+
+    updateScreen();
+}
+
+ScreenController();
+
